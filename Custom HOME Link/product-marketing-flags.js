50ca53796b45ddcbe0cadb3d13edfd3c9d82afce
Chris Bradley <chris@myprojectsolution.net>
2023-02-07 19:44:46 -0500

    Create product-marketing-flags.js
    Adds image icons over corners of primary product image. Replaces merchandising bundle add-on.

---
+++ Custom HOME Link/product-marketing-flags.js
@@ -0,0 +1,21 @@
+<script type="text/html" id="catalog.flags">
+  <!-- ko if: searchfields["searchfield3"].show() -->
+  <div id="flag1" class="product_flags">
+	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield1'].value()) }" />
+  </div>
+  <!-- /ko -->
+  <!-- ko if: searchfields["searchfield4"].show() -->
+  <div id="flag2" class="product_flags">
+	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield2'].value()) }" />
+  </div>
+  <!-- /ko --><!-- ko if: searchfields["searchfield3"].show() -->
+  <div id="flag3" class="product_flags">
+	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield3'].value()) }" />
+  </div>
+  <!-- /ko -->
+  <!-- ko if: searchfields["searchfield4"].show() -->
+  <div id="flag4" class="product_flags">
+	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield4'].value()) }" />
+  </div>
+  <!-- /ko -->
+</script>
