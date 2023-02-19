ad1d53b204370c71989c51ac98ac25d0b8b73231
Chris Bradley <chris@myprojectsolution.net>
2023-02-19 11:10:00 -0500

    Update product-marketing-flags.js
    Updated searchfield numbers in ko if

--- Product Marketing Flags/product-marketing-flags.js
+++ Product Marketing Flags/product-marketing-flags.js
@@ -1,10 +1,10 @@
 <script type="text/html" id="catalog.flags">
-  <!-- ko if: searchfields["searchfield3"].show() -->
+  <!-- ko if: searchfields["searchfield1"].show() -->
   <div id="flag1" class="product_flags">
 	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield1'].value()) }" />
   </div>
   <!-- /ko -->
-  <!-- ko if: searchfields["searchfield4"].show() -->
+  <!-- ko if: searchfields["searchfield2"].show() -->
   <div id="flag2" class="product_flags">
 	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield2'].value()) }" />
   </div>
