03ec9b4610e86ca975432d47f513d49dbbc59d72
Jason Bagwell <88937690+jbmps@users.noreply.github.com>
2023-02-22 08:38:54 -0500

    Updated to use text instead of images

--- Product Marketing Flags/product-marketing-flags.js
+++ Product Marketing Flags/product-marketing-flags.js
@@ -2,22 +2,47 @@
 <script type="text/html" id="catalog.flags">
   <!-- ko if: searchfields["searchfield1"] && searchfields["searchfield1"].show() -->
   <div id="flag1" class="product_flags">
-	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield1'].value()) }" />
+    <span data-bind="text: searchfields['searchfield1'].value()"></span>
   </div>
   <!-- /ko -->
   <!-- ko if: searchfields["searchfield2"] && searchfields["searchfield2"].show() -->
   <div id="flag2" class="product_flags">
-	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield2'].value()) }" />
+	<span data-bind="text: searchfields['searchfield2'].value()"></span>
   </div>
   <!-- /ko -->
   <!-- ko if: searchfields["searchfield3"] && searchfields["searchfield3"].show() -->
   <div id="flag3" class="product_flags">
-	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield3'].value()) }" />
+	<span data-bind="text: searchfields['searchfield3'].value()"></span>
   </div>
   <!-- /ko -->
   <!-- ko if: searchfields["searchfield4"] && searchfields["searchfield4"].show() -->
   <div id="flag4" class="product_flags">
-	<img width="50px" data-bind="attr: {src: utils.buildImagePath(searchfields['searchfield4'].value()) }" />
+	<span data-bind="text: searchfields['searchfield4'].value()"></span>
   </div>
   <!-- /ko -->
 </script>
+
+<style>
+  .product_flags{
+  	font-family: 'Heebo';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 10px;
+    border-radius: 5px;
+    padding: 2px 8px;
+    color: #fff;
+  }
+  div#flag1 {
+    background-color: #D20000;
+  }
+  div#flag2 {
+    background-color: #FFC700;
+  	color: #000;
+  }
+  div#flag3 {
+    background-color: #A39161;
+  }
+  div#flag4 {
+    background-color: #008C99;
+  }
+</style>
