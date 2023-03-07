b5fbaff9e03ef90113d632df97dad79a78fd5fc6
Jason Bagwell <88937690+jbmps@users.noreply.github.com>
2023-03-07 08:20:52 -0500

    added search results page template override.

--- Product Marketing Flags/product-marketing-flags.js
+++ Product Marketing Flags/product-marketing-flags.js
@@ -1,4 +1,3 @@
-
 <script type="text/html" id="catalog.flags">
   <!-- ko if: searchfields["searchfield1"] && searchfields["searchfield1"].show() -->
   <div id="flag1" class="product_flags">
@@ -21,6 +20,30 @@
   </div>
   <!-- /ko -->
 </script>
+
+<script type="text/html" id="catalog.flags_thumb">
+  <!-- ko if: searchfields["searchfield1"] && searchfields["searchfield1"].show() -->
+  <div id="flag1" class="product_flags">
+    <span data-bind="text: searchfields['searchfield1'].value()"></span>
+  </div>
+  <!-- /ko -->
+  <!-- ko if: searchfields["searchfield2"] && searchfields["searchfield2"].show() -->
+  <div id="flag2" class="product_flags">
+	<span data-bind="text: searchfields['searchfield2'].value()"></span>
+  </div>
+  <!-- /ko -->
+  <!-- ko if: searchfields["searchfield3"] && searchfields["searchfield3"].show() -->
+  <div id="flag3" class="product_flags">
+	<span data-bind="text: searchfields['searchfield3'].value()"></span>
+  </div>
+  <!-- /ko -->
+  <!-- ko if: searchfields["searchfield4"] && searchfields["searchfield4"].show() -->
+  <div id="flag4" class="product_flags">
+	<span data-bind="text: searchfields['searchfield4'].value()"></span>
+  </div>
+  <!-- /ko -->
+</script>
+
 
 <style>
   .product_flags{
